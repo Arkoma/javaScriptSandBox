e6759e8d624f206a88241ebc226661675b90f47c
Aaron <aarondburk@gmail.com>
2017-11-05 22:00:26 -0600

    Documentation update
    Updated the documentationon the chessboard.js exercise

--- chessboard.js
+++ chessboard.js
@@ -1,3 +1,31 @@
+/***
+ *      /$$$$$$  /$$$$$$$  /$$$$$$$  /$$   /$$ /$$$$$$$  /$$   /$$                           /$$    
+ *     /$$__  $$| $$__  $$| $$__  $$| $$  | $$| $$__  $$| $$  /$$/                          | $$    
+ *    | $$  \ $$| $$  \ $$| $$  \ $$| $$  | $$| $$  \ $$| $$ /$$/      /$$$$$$$   /$$$$$$  /$$$$$$  
+ *    | $$$$$$$$| $$  | $$| $$$$$$$ | $$  | $$| $$$$$$$/| $$$$$/      | $$__  $$ /$$__  $$|_  $$_/  
+ *    | $$__  $$| $$  | $$| $$__  $$| $$  | $$| $$__  $$| $$  $$      | $$  \ $$| $$$$$$$$  | $$    
+ *    | $$  | $$| $$  | $$| $$  \ $$| $$  | $$| $$  \ $$| $$\  $$     | $$  | $$| $$_____/  | $$ /$$
+ *    | $$  | $$| $$$$$$$/| $$$$$$$/|  $$$$$$/| $$  | $$| $$ \  $$ /$$| $$  | $$|  $$$$$$$  |  $$$$/
+ *    |__/  |__/|_______/ |_______/  \______/ |__/  |__/|__/  \__/|__/|__/  |__/ \_______/   \___/  
+ *                                                                                                  
+ *                                                                                                  
+ * 
+ * This is a javascript challenge from Marijn Haverbeke's book, Eloquent JavaScript.  This one is
+ * called the chessboard, because it is written to output to the console log a grid using spaces 
+ * and hashtags.  
+ * 
+ * It does this by having a for loop nested inside another for loop. The odd lines using the
+ * variable, "oddLine" creates a pattern of one space followed by one hashtag alternating until
+ * the line reaches the length input by the user which has been assigned to the variable "size."
+ * The variable "evenLine" uses the same conditional statemenst only changing the pattern by 
+ * starting with the hashtag and then the space. The chessboard variable then repeats this 
+ * pattern back and forth with odd lines and even lines until the number of lines matches the 
+ * same variable "size."
+ * 
+ * This one took several tries for me to finally get it right.
+ * 
+ */
+
 var size = prompt("What size chessboard would you like? \(a standard board is 8x8\)","8")
 if ((!isNaN(size)) && size < 51) {
 var chessBoard = "";
