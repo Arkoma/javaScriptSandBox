6ec3fd5d0ab2937f5ad3536e2e0fef2452db2420
Aaron <aarondburk@gmail.com>
2017-12-28 21:30:00 -0600

    Yet another way of fixing the mistake by putting the conditional and loop within the function that is being passed within the outer function

--- assignTorpedo.js
+++ assignTorpedo.js
@@ -1,15 +1,16 @@
-    function assignTorpedo ( name, passengerArray ){
-        for ( i = 0; i < passengerArray.length; i++ ) {
-            if ( passengerArray[i] == name ) {
-                return function () { //By returning the function here
-                    alert(`Ahoy, ${name}! 
-                    Man your post at torpedo #${i +1}!`);
-                };
+function makeTorpedoAssigner ( passengerArray ){ //change function's name from assignTorpedo to makeTorpedoAssigner and remove the name parameter
+    return function ( name ) {
+        for (var i = 0; i < passengerArray.length; i++ ) {
+            if (passengerArray[i] == name) {  //passengerArray is the only external value inserted into this function, which is apparently good, though I'm not 100% certain why
+                alert(
+`Ahoy, ${name}!
+Man your post at torpedo #${i+1}!`);
             }
-        } //instead of here using the torpedoAssignment variable, we lock in the value of i at the proper time within the conditional if statement.  This is one way to fix the problem.
-    }
+        }
+    };
+}
 
     var subPassengers = ["Luke", "Leia", "Han", "Chewie", "Yoda", "R2-D2", "C-3PO", "Boba"];
-    var giveAssignment = assignTorpedo ( "Chewie", subPassengers );
+    var giveAssignment = makeTorpedoAssigner ( subPassengers );
 
-    giveAssignment();
+    giveAssignment('Chewie');
