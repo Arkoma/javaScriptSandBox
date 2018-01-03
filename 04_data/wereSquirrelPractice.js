eeda831a7055e781b0658d648944a7a29721c568
Aaron <aarondburk@gmail.com>
2018-01-03 13:16:17 -0600

    more additions to the code...I think

--- 04_data/wereSquirrelPractice.js
+++ 04_data/wereSquirrelPractice.js
@@ -5,3 +5,19 @@ for (var event in correlations) {
   if (correlation > 0.1 || correlation < -0.1)
     console.log(event + ": " + correlation);
 }
+
+function hasEvent(event, entry) {
+  return entry.events.indexOf(event) != -1;
+}
+
+function tableFor(event, journal) {
+  var table = [0, 0, 0, 0];
+  for (var i = 0; i < journal.length; i++) {
+    var entry = journal[i], index = 0;
+    if (hasEvent(event, entry)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+consloe.log(tableFor('pizza', journal));
