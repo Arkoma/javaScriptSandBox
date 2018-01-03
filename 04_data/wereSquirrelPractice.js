bf7e1f92c665a18dcee0695a8a5dee567a90747b
Aaron <aarondburk@gmail.com>
2018-01-03 14:07:10 -0600

    looking at a map data type

--- 04_data/wereSquirrelPractice.js
+++ 04_data/wereSquirrelPractice.js
@@ -20,4 +20,17 @@ function tableFor(event, journal) {
   }
   return table;
 }
-consloe.log(tableFor('pizza', journal));
+// console.log(tableFor('pizza', journal));
+
+var map = {};
+function storePhi(event, phi) {
+  map[event] = phi;
+}
+
+storePhi('pizza', 0.069);
+storePhi('touched tree', -0.081);
+// console.log('pizza' in map);
+// console.log(map['touched tree']);
+
+for (var event in map)
+  console.log("The correlation for '" + event + "' is " + map[event]);
